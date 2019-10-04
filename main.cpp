3acfe38d4796a1e3b143b2bedb64be3ae4f5bbf1
daramirez21 <daramirez@mail.csuchico.edu>
2019-10-04 11:46:44 -0700

    Fully Works

--- main.cpp
+++ main.cpp
@@ -6,75 +6,61 @@ using namespace std;
 
 int main()
 {
-    char choice = 'y';
-
-    do {
-
         string fileName; //to get the name of the file to open
         string line; //to store a single line of a text file
         fstream fileStream; //to open a file for reading
 
         int nlines;
         int characters;
-
-        nlines = 0;
-        characters = 0;  
-
+        char choice;
+         
+        do {
         cout<<"What file do you want to open? ";
         getline(cin, fileName);
 
-        //STEP 1: open the fileStream for input, using the fileName specified
+        characters = 0;
+        nlines = 0;
 
         fileStream.open( fileName.c_str(), ios::in );
- 
-        if( fileStream.is_open() )
+    
+        if( fileStream.is_open())
         {
                 cout<<fileName<<" opened.\nFILE CONTENTS:\n";
-
-                while (!fileStream.eof() )
+                   
+                while(!fileStream.eof())
                 {
-
                         getline( fileStream, line);
 
-                        if(!line.empty() )
+                        if(!line.empty())
                         {
+                           nlines++;
+                           characters += line.length();
 
-                                cout << line << endl;
-
-                                nlines = nlines + 1;
-                                        
-                                characters = characters + line.length();
+                           cout<<line<<endl;
                         }
-
+                }
+               
+                cout<< "METADATA" << endl;
+                cout<< "File: " << endl;
+                cout<< "Lines: " << nlines << endl;
+                cout<< "Characters: " << characters << endl;
+                fileStream.close();
+               
+                }
+                else 
+                {
+                 cout << fileName << " could not be opened.\n";
+                 cout << "METADATA" << endl;
+                 cout << "File: " << fileName << endl;
+                 cout << "Lines: "<< nlines << endl;
+                 cout << "Characters: " << characters << endl;
                 }
 
-                cout << "METADATA" << endl;
-
-                cout << "File: " << fileName << endl;
-
-                cout << "Lines: "<< nlines << endl;
-
-                cout << "Characters: " << characters << endl;
-
-                cout << "Analyze another file (y/n)? " << endl;
+                cout << "Analyze another file (y/n)?" <<endl;
                 cin >> choice;
                 cin.ignore();
-
-                  }
- 
-        else
-        {
-                cout << fileName <<" could not be opened.\n";
-                cout << "METADATA" << endl;
-                cout << "File: " <<fileName<<endl;
-                cout << "Lines: " <<nlines << endl;
-                cout << "Characters: " <<characters<<endl;
-                cout << "Analyze another file (y/n)? " <<endl;
-                cin >> choice;
-                cin.ignore();
-                         
-         }
-  }  while( choice=='y' || choice == 'Y' );
+  
+  }  while( choice== 'y' || choice == 'Y' );
 
     return 0;
 }
